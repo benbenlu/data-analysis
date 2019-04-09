7b95c058a403141629dc8d57516e91b9904efeaa
wangll <wangululu@iboxpay.com>
2019-04-09 17:52:06 +0800

    titanic数据查看生存与性别之间的关系

--- python-basic/02_dataType.py
+++ python-basic/02_dataType.py
@@ -1,5 +1,19 @@
+#!/Users/wanglulu/anaconda3/bin/python3
 import unicodecsv
+from collections import Iterator
+# myList = []
+# with open('../analysis_process_01/csv/test.csv', 'rb') as f:
+#     reader = unicodecsv.DictReader(f)
+#     myList = list(reader)
 
-with open('../analysis_process_01/csv/test.csv', 'rb+') as f:
-    reader = unicodecsv.DictReader(f)
-    
+# print('myList', myList)
+
+def f(x):
+    return x * x
+
+r = map(f, [1, 2, 3, 4, 5, 6]) # 返回的是迭代器
+print('r', r)
+print('r.next()', isinstance(r, Iterator))
+print('r', next(r))
+print('r', next(r))
+print('r', list(r))
